5851d24b85ac43a3738b96143024b35451e91edc
kevinle108 <kevinle108@gmail.com>
2021-01-22 01:10:08 -0500

    pokemon api

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
 /**
  * This is the main JavaScript entry for your application.
  */
+
+let xhr = new XMLHttpRequest();
+xhr.onreadystatechange = (evt) => {
+   if (xhr.readyState == 4) {
+      const response = xhr.responseText;
+      const data = JSON.parse(response)
+      console.log(data.sprites.front_default);
+      document.getElementById("pokemonImage").src = data.sprites.front_default
+   }
+   
+};
+
+
+
+document.getElementById("pokeButton").addEventListener("click", changeSprite);
+
+function changeSprite() {
+   xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + document.getElementById("pokemonInput").value);
+   xhr.send();
+}
